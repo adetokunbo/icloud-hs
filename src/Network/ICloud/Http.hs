dff9ecadfb9c3f2f60e0d0715e19d11361990be5
Tim Emiola <adetokunbo@users.noreply.github.com>
2025-03-26 14:05:40 +0900

    simplify staticHeaders; extract xAppleKey

--- src/Network/ICloud/Http.hs
+++ src/Network/ICloud/Http.hs
@@ -287,11 +287,15 @@ updateSessionData hs sd =
 
 staticHeaders :: [Header]
 staticHeaders =
-  [ ("X-Apple-OAuth-Client-Id", "d39ba9916b7251055b22c7f910e2ea796ee65e98b2ddecea8f5dde8d9d1a815d")
+  [ ("X-Apple-OAuth-Client-Id", xAppleKey)
   , ("X-Apple-OAuth-Client-Type", "firstPartyAuth")
   , ("X-Apple-OAuth-Redirect-URI", "https://www.icloud.com")
   , ("X-Apple-OAuth-Require-Grant-Code", "true")
   , ("X-Apple-OAuth-Response-Mode", "web_message")
   , ("X-Apple-OAuth-Response-Type", "code")
-  , ("X-Apple-Widget-Key", "d39ba9916b7251055b22c7f910e2ea796ee65e98b2ddecea8f5dde8d9d1a815d")
+  , ("X-Apple-Widget-Key", xAppleKey)
   ]
+
+
+xAppleKey :: ByteString
+xAppleKey = "d39ba9916b7251055b22c7f910e2ea796ee65e98b2ddecea8f5dde8d9d1a815d"
