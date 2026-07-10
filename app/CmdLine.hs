bb19cbd199a53db47568e2e2a1bfb72f42ba2d06
Tim Emiola <adetokunbo@users.noreply.github.com>
2026-07-10 09:46:19 +0900

    Allow a default log location

--- app/CmdLine.hs
+++ app/CmdLine.hs
@@ -3,47 +3,110 @@
 module Main where
 
 import Control.Exception (catch, displayException)
+import Data.String (fromString)
 import Network.HTTP.Client.TLS (newTlsManager)
 import Network.ICloud.Http (AuthError, fileLogger, login, mkApiWith, withLogger)
 import Network.ICloud.Http.Endpoints (Realm (..), realmEndpoints)
-import Network.ICloud.Session (loadSession)
+import Network.ICloud.Session (Credentials (..), loadSession, saveCredentials)
 import Options.Applicative
+import System.Directory (createDirectoryIfMissing)
+import System.Environment.XDG.BaseDir (getUserCacheDir)
 import System.Exit (exitFailure)
-import System.IO (IOMode (..), withFile)
+import System.FilePath ((</>))
+import System.IO (IOMode (..), hFlush, hSetEcho, stdin, stdout, withFile)
 
 
-data Opts = Opts
-  { optChina :: Bool
-  , optLog :: Maybe FilePath
+data Command
+  = Init
+  | Login LoginOpts
+
+
+data LoginOpts = LoginOpts
+  { loginChina :: Bool
+  , loginLog :: Bool
+  , loginLogFile :: Maybe FilePath
   }
 
 
-optsParser :: Parser Opts
-optsParser =
-  Opts
+commandParser :: Parser Command
+commandParser =
+  (Init <$ subparser (command "init" (info (pure ()) (progDesc "Save Apple ID credentials to the config directory"))))
+    <|> (Login <$> loginOptsParser)
+
+
+loginOptsParser :: Parser LoginOpts
+loginOptsParser =
+  LoginOpts
     <$> switch (long "china" <> help "Use mainland China endpoints")
-    <*> optional (strOption (long "log" <> metavar "FILE" <> help "Append HTTP exchanges to FILE"))
+    <*> switch (long "log" <> help "Append HTTP exchanges to the default log file")
+    <*> optional (strOption (long "log-file" <> metavar "FILE" <> help "Append HTTP exchanges to FILE"))
 
 
-cliParser :: ParserInfo Opts
+cliParser :: ParserInfo Command
 cliParser =
   info
-    (optsParser <**> helper)
-    (fullDesc <> progDesc "Sign in to iCloud and cache the session token")
+    (commandParser <**> helper)
+    (fullDesc <> progDesc "icloud-auth: iCloud authentication tool")
 
 
 main :: IO ()
-main = do
-  opts <- execParser cliParser
+main =
+  execParser cliParser >>= \case
+    Init -> runInit
+    Login opts -> runLogin opts
+
+
+runInit :: IO ()
+runInit = do
+  appleId <- prompt "Apple ID: "
+  password <- promptSecret "Password: "
+  saveCredentials (Credentials (fromString appleId) (fromString password))
+  putStrLn "Credentials saved."
+
+
+runLogin :: LoginOpts -> IO ()
+runLogin opts = do
   session <- loadSession
   mgr <- newTlsManager
-  let realm = if optChina opts then China else Usual
+  let realm = if loginChina opts then China else Usual
   api0 <- mkApiWith session (realmEndpoints realm) mgr
-  let runLogin api = login api >> putStrLn "Authenticated."
-  let go = case optLog opts of
-        Nothing -> runLogin api0
+  mbLogPath <- resolveLogTarget opts
+  let go = case mbLogPath of
+        Nothing -> login api0 >> putStrLn "Authenticated."
         Just fp -> withFile fp AppendMode $ \h ->
-          runLogin (withLogger (fileLogger h) api0)
+          login (withLogger (fileLogger h) api0) >> putStrLn "Authenticated."
   go `catch` \e -> do
     putStrLn $ "Login failed: " <> displayException (e :: AuthError)
     exitFailure
+
+
+resolveLogTarget :: LoginOpts -> IO (Maybe FilePath)
+resolveLogTarget LoginOpts{loginLogFile = Just fp} = pure (Just fp)
+resolveLogTarget LoginOpts{loginLog = True} = Just <$> defaultLogFile
+resolveLogTarget _ = pure Nothing
+
+
+defaultLogFile :: IO FilePath
+defaultLogFile = do
+  dir <- getUserCacheDir appDir
+  createDirectoryIfMissing True dir
+  pure (dir </> "requests.log")
+
+
+appDir :: FilePath
+appDir = "hs-icloud-auth"
+
+
+prompt :: String -> IO String
+prompt label = putStr label >> hFlush stdout >> getLine
+
+
+promptSecret :: String -> IO String
+promptSecret label = do
+  putStr label
+  hFlush stdout
+  hSetEcho stdin False
+  secret <- getLine
+  hSetEcho stdin True
+  putStrLn ""
+  pure secret
