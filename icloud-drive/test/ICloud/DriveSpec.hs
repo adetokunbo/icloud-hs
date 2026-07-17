a23327b105471c687b7b406867b0857d91f8a338
Tim Emiola <adetokunbo@users.noreply.github.com>
2026-07-17 20:55:34 +0900

    icloud-drive: add mock HTTP integration tests

--- icloud-drive/test/ICloud/DriveSpec.hs
+++ icloud-drive/test/ICloud/DriveSpec.hs
@@ -2,9 +2,204 @@
 
 module ICloud.DriveSpec (spec) where
 
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BS8
+import qualified Data.ByteString.Lazy as LBS
+import qualified Data.ByteString.Lazy.Char8 as LBS8
+import Data.IORef (IORef, newIORef, readIORef, writeIORef)
+import qualified Data.Map.Strict as Map
+import qualified Data.Text as Text
+import Network.HTTP.Client (Request (..), defaultManagerSettings, defaultRequest, newManager)
+import Network.HTTP.Types (HeaderName, hContentType, methodPost, status200, status404)
+import Network.ICloud.Drive
+import Network.ICloud.Http (Api, mkApiWith)
+import Network.ICloud.Http.Endpoints (Endpoints (..))
+import Network.ICloud.Session (AccountData (..), Credentials (..), Session (..))
+import Network.Wai (Application, rawPathInfo, responseLBS)
+import Network.Wai.Handler.Warp (testWithApplication)
+import System.IO.Temp (withSystemTempDirectory)
 import Test.Hspec
 
 
 spec :: Spec
 spec = describe "Network.ICloud.Drive" $ do
-  it "placeholder" $ (pure () :: IO ())
+  describe "driveRoot" $ do
+    it "returns root FolderData" $
+      withNodeMock rootJson $ \ep api -> do
+        fd <- driveRoot api ep
+        fnId fd `shouldBe` DriveNodeId "FOLDER::com.apple.CloudDocs::root"
+
+  describe "listFolder" $ do
+    it "returns all children" $
+      withNodeMock subfolderJson $ \ep api -> do
+        nodes <- listFolder api ep (DriveNodeId "FOLDER::com.apple.CloudDocs::D5AA0425")
+        length nodes `shouldBe` 2
+    it "returns DriveFile nodes for file children" $
+      withNodeMock subfolderJson $ \ep api -> do
+        nodes <- listFolder api ep (DriveNodeId "FOLDER::com.apple.CloudDocs::D5AA0425")
+        all isFile nodes `shouldBe` True
+
+  describe "downloadFile" $ do
+    it "returns LBS.empty for a zero-size file" $
+      withNodeMock rootJson $ \ep api -> do
+        let fd = testFileData{fdSize = Nothing}
+        downloadFile api ep fd `shouldReturn` LBS.empty
+    it "downloads file contents" $
+      withDownloadMock $ \ep api ->
+        downloadFile api ep testFileData `shouldReturn` "test file content"
+
+
+-- Mock servers
+
+withNodeMock :: LBS.ByteString -> (DriveEndpoints -> Api -> IO a) -> IO a
+withNodeMock nodeJson action =
+  withSystemTempDirectory "icloud-drive-mock" $ \tmpDir ->
+    testWithApplication (pure (nodeApp nodeJson)) $ \serverPort -> do
+      (ep, api) <- mkEpAndApi serverPort tmpDir
+      action ep api
+
+
+withDownloadMock :: (DriveEndpoints -> Api -> IO a) -> IO a
+withDownloadMock action =
+  withSystemTempDirectory "icloud-drive-download" $ \tmpDir -> do
+    portRef <- newIORef 0
+    testWithApplication (pure (downloadApp portRef)) $ \serverPort -> do
+      writeIORef portRef serverPort
+      (ep, api) <- mkEpAndApi serverPort tmpDir
+      action ep api
+
+
+nodeApp :: LBS.ByteString -> Application
+nodeApp nodeJson req respond
+  | "/retrieveItemDetailsInFolders" `BS.isSuffixOf` rawPathInfo req =
+      respond $ responseLBS status200 jsonHeaders nodeJson
+  | otherwise =
+      respond $ responseLBS status404 [] "not found"
+
+
+downloadApp :: IORef Int -> Application
+downloadApp portRef req respond = do
+  serverPort <- readIORef portRef
+  let p = rawPathInfo req
+  if "/download/by_id" `BS.isSuffixOf` p
+    then
+      let url = "http://127.0.0.1:" ++ show serverPort ++ "/content/test"
+          body = LBS8.pack $ "{\"data_token\":{\"url\":\"" ++ url ++ "\",\"token\":\"tok\"}}"
+       in respond $ responseLBS status200 jsonHeaders body
+    else
+      if p == "/content/test"
+        then respond $ responseLBS status200 [] "test file content"
+        else respond $ responseLBS status404 [] "not found"
+
+
+mkEpAndApi :: Int -> FilePath -> IO (DriveEndpoints, Api)
+mkEpAndApi serverPort tmpDir = do
+  let baseUrl = Text.pack $ "http://127.0.0.1:" ++ show serverPort
+  ep <- mkDriveEndpoints (testAccountData baseUrl) (testSession tmpDir)
+  mgr <- newManager defaultManagerSettings
+  api <- mkApiWith (testSession tmpDir) (testAuthEndpoints serverPort) mgr
+  pure (ep, api)
+
+
+-- Fixtures
+
+testAccountData :: Text.Text -> AccountData
+testAccountData baseUrl =
+  AccountData
+    { adHsaVersion = 2
+    , adHsaChallengeRequired = False
+    , adHsaTrustedBrowser = True
+    , adWebservices = Map.fromList [("drivews", baseUrl), ("docws", baseUrl)]
+    }
+
+
+testSession :: FilePath -> Session
+testSession topDir =
+  Session
+    { sessionCreds = Credentials{credAccountName = "test@example.com", credPassword = "test-pass"}
+    , sessionTopDir = topDir
+    , sessionClientId = "auth-test-client-id"
+    }
+
+
+testAuthEndpoints :: Int -> Endpoints
+testAuthEndpoints serverPort =
+  Endpoints
+    { epHome = "http://127.0.0.1:" <> BS8.pack (show serverPort)
+    , epAuth = dummyReq "/appleauth/auth"
+    , epSetup = dummyReq "/setup/ws/1"
+    }
+ where
+  dummyReq reqPath =
+    defaultRequest
+      { host = "127.0.0.1"
+      , port = serverPort
+      , secure = False
+      , method = methodPost
+      , path = reqPath
+      }
+
+
+testFileData :: FileData
+testFileData =
+  FileData
+    { fdId = DriveNodeId "FILE::com.apple.CloudDocs::33A41112"
+    , fdDocId = "33A41112"
+    , fdEtag = "2k::2j"
+    , fdName = "Scan 2"
+    , fdExtension = Just "pdf"
+    , fdZone = "com.apple.CloudDocs"
+    , fdSize = Just 19876991
+    , fdDateCreated = Nothing
+    , fdDateModified = Nothing
+    }
+
+
+isFile :: DriveNode -> Bool
+isFile (DriveFile _) = True
+isFile _ = False
+
+
+jsonHeaders :: [(HeaderName, BS8.ByteString)]
+jsonHeaders = [(hContentType, "application/json")]
+
+
+rootJson :: LBS.ByteString
+rootJson =
+  "[{\"drivewsid\":\"FOLDER::com.apple.CloudDocs::root\"\
+  \,\"zone\":\"com.apple.CloudDocs\"\
+  \,\"name\":\"\"\
+  \,\"etag\":\"31\"\
+  \,\"type\":\"FOLDER\"\
+  \}]"
+
+
+subfolderJson :: LBS.ByteString
+subfolderJson =
+  "[{\"drivewsid\":\"FOLDER::com.apple.CloudDocs::D5AA0425-E84F-4501-AF5D-60F1D92648CF\"\
+  \,\"zone\":\"com.apple.CloudDocs\"\
+  \,\"name\":\"Test\"\
+  \,\"etag\":\"2z\"\
+  \,\"type\":\"FOLDER\"\
+  \,\"items\":[\
+  \{\"drivewsid\":\"FILE::com.apple.CloudDocs::33A41112-4131-4938-9691-7F356CE3C51D\"\
+  \,\"docwsid\":\"33A41112-4131-4938-9691-7F356CE3C51D\"\
+  \,\"zone\":\"com.apple.CloudDocs\"\
+  \,\"name\":\"Scan 2\"\
+  \,\"dateModified\":\"2020-04-27T21:37:36Z\"\
+  \,\"size\":19876991\
+  \,\"etag\":\"2k::2j\"\
+  \,\"extension\":\"pdf\"\
+  \,\"type\":\"FILE\"\
+  \}\
+  \,{\"drivewsid\":\"FILE::com.apple.CloudDocs::516C896C-6AA5-4A30-B30E-5502C2333DAE\"\
+  \,\"docwsid\":\"516C896C-6AA5-4A30-B30E-5502C2333DAE\"\
+  \,\"zone\":\"com.apple.CloudDocs\"\
+  \,\"name\":\"Scanned document 1\"\
+  \,\"dateModified\":\"2020-05-03T00:15:17Z\"\
+  \,\"size\":21644358\
+  \,\"etag\":\"32::2x\"\
+  \,\"extension\":\"pdf\"\
+  \,\"type\":\"FILE\"\
+  \}\
+  \]}]"
