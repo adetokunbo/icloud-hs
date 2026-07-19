f96fa99936c726f24d7827ac26e6b9dd78c0bcfe
Tim Emiola <adetokunbo@users.noreply.github.com>
2026-07-19 13:56:57 +0900

    icloud-notes: add CloudKit wire types and JSON parsing tests

--- icloud-notes/src-internal/Network/ICloud/Internal/Notes/CloudKit.hs
+++ icloud-notes/src-internal/Network/ICloud/Internal/Notes/CloudKit.hs
@@ -1,1 +1,253 @@
-module Network.ICloud.Internal.Notes.CloudKit where
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE TypeFamilies #-}
+
+module Network.ICloud.Internal.Notes.CloudKit
+  ( CKZoneId (..)
+  , CKRecordRef (..)
+  , CKAsset (..)
+  , CKTimestamp (..)
+  , CKField (..)
+  , CKRecord (..)
+  , CKQueryResponse (..)
+  , CKLookupResponse (..)
+  , CKZoneChangesZone (..)
+  , CKZoneChangesResponse (..)
+  )
+where
+
+import Control.Monad (guard)
+import Data.Aeson
+  ( FromJSON (..)
+  , Object
+  , Value
+  , withObject
+  , (.!=)
+  , (.:)
+  , (.:?)
+  )
+import Data.Aeson.Types (Parser)
+import Data.Foldable (asum)
+import Data.Int (Int64)
+import Data.Map.Strict (Map)
+import Data.Proxy (Proxy (..))
+import Data.Text (Text, pack)
+import GHC.TypeLits (KnownSymbol, Symbol, symbolVal)
+
+
+data CKZoneId = CKZoneId
+  { czName :: Text
+  , czType :: Text
+  }
+  deriving (Eq, Show)
+
+
+instance FromJSON CKZoneId where
+  parseJSON = withObject "CKZoneId" $ \o ->
+    CKZoneId
+      <$> o .: "zoneName"
+      <*> o .: "zoneType"
+
+
+data CKRecordRef = CKRecordRef
+  { rrRecordName :: Text
+  , rrAction :: Text
+  }
+  deriving (Eq, Show)
+
+
+instance FromJSON CKRecordRef where
+  parseJSON = withObject "CKRecordRef" $ \o ->
+    CKRecordRef
+      <$> o .: "recordName"
+      <*> o .: "action"
+
+
+data CKAsset = CKAsset
+  { caDownloadUrl :: Text
+  , caFileChecksum :: Text
+  , caRefChecksum :: Text
+  , caWrappingKey :: Text
+  , caSize :: Int64
+  }
+  deriving (Eq, Show)
+
+
+instance FromJSON CKAsset where
+  parseJSON = withObject "CKAsset" $ \o ->
+    CKAsset
+      <$> o .: "downloadURL"
+      <*> o .: "fileChecksum"
+      <*> o .: "referenceChecksum"
+      <*> o .: "wrappingKey"
+      <*> o .: "size"
+
+
+data CKTimestamp = CKTimestamp
+  { ctTimestamp :: Int64
+  , ctUserRecordName :: Text
+  }
+  deriving (Eq, Show)
+
+
+instance FromJSON CKTimestamp where
+  parseJSON = withObject "CKTimestamp" $ \o ->
+    CKTimestamp
+      <$> o .: "timestamp"
+      <*> o .: "userRecordName"
+
+
+-- Internal newtypes: give distinct Haskell types to CK tags that share a
+-- primitive (Text covers "STRING"/"ENCRYPTED_BYTES"; Int64 covers
+-- "INT64"/"TIMESTAMP"), making CKFieldTag a total function.
+newtype CKString = CKString Text
+  deriving (FromJSON)
+
+
+newtype CKEncryptedBytes = CKEncryptedBytes Text
+  deriving (FromJSON)
+
+
+newtype CKInt64Value = CKInt64Value Int64
+  deriving (FromJSON)
+
+
+newtype CKTimestampValue = CKTimestampValue Int64
+  deriving (FromJSON)
+
+
+-- Single source of truth mapping each value type to its CloudKit "type" tag.
+type family CKFieldTag a :: Symbol where
+  CKFieldTag CKString = "STRING"
+  CKFieldTag CKInt64Value = "INT64"
+  CKFieldTag CKTimestampValue = "TIMESTAMP"
+  CKFieldTag CKEncryptedBytes = "ENCRYPTED_BYTES"
+  CKFieldTag CKRecordRef = "REFERENCE"
+  CKFieldTag [CKRecordRef] = "REFERENCE_LIST"
+  CKFieldTag CKAsset = "ASSETID"
+
+
+-- Confirms the pre-parsed "type" tag matches CKFieldTag a, then parses "value".
+matchField
+  :: forall a
+   . (KnownSymbol (CKFieldTag a), FromJSON a)
+  => Text
+  -> Object
+  -> Parser a
+matchField typ o = do
+  guard (typ == pack (symbolVal (Proxy :: Proxy (CKFieldTag a))))
+  o .: "value"
+
+
+data CKField
+  = CKStringField Text
+  | CKInt64Field Int64
+  | CKTimestampField Int64
+  | CKEncryptedBytesField Text
+  | CKReferenceField CKRecordRef
+  | CKReferenceListField [CKRecordRef]
+  | CKAssetIdField CKAsset
+  | CKUnknownField Text Value
+  deriving (Eq, Show)
+
+
+instance FromJSON CKField where
+  parseJSON = withObject "CKField" $ \o -> do
+    typ <- o .: "type" :: Parser Text
+    asum
+      [ CKStringField . (\(CKString t) -> t) <$> matchField typ o
+      , CKInt64Field . (\(CKInt64Value i) -> i) <$> matchField typ o
+      , CKTimestampField . (\(CKTimestampValue i) -> i) <$> matchField typ o
+      , CKEncryptedBytesField . (\(CKEncryptedBytes t) -> t) <$> matchField typ o
+      , CKReferenceField <$> matchField typ o
+      , CKReferenceListField <$> matchField typ o
+      , CKAssetIdField <$> matchField typ o
+      , CKUnknownField typ <$> o .: "value"
+      ]
+
+
+data CKRecord = CKRecord
+  { crName :: Text
+  , crType :: Maybe Text
+  , crChangeTag :: Maybe Text
+  , crZoneId :: Maybe CKZoneId
+  , crFields :: Map Text CKField
+  , crCreated :: Maybe CKTimestamp
+  , crModified :: Maybe CKTimestamp
+  , crDeleted :: Maybe Bool
+  }
+  deriving (Eq, Show)
+
+
+instance FromJSON CKRecord where
+  parseJSON = withObject "CKRecord" $ \o ->
+    CKRecord
+      <$> o .: "recordName"
+      <*> o .:? "recordType"
+      <*> o .:? "recordChangeTag"
+      <*> o .:? "zoneID"
+      <*> o .:? "fields" .!= mempty
+      <*> o .:? "created"
+      <*> o .:? "modified"
+      <*> o .:? "deleted"
+
+
+data CKQueryResponse = CKQueryResponse
+  { qrRecords :: [CKRecord]
+  , qrContinuationMarker :: Maybe Value
+  }
+  deriving (Eq, Show)
+
+
+instance FromJSON CKQueryResponse where
+  parseJSON = withObject "CKQueryResponse" $ \o ->
+    CKQueryResponse
+      <$> o .: "records"
+      <*> o .:? "continuationMarker"
+
+
+data CKLookupResponse = CKLookupResponse
+  { lrRecords :: [CKRecord]
+  , lrSyncToken :: Maybe Text
+  }
+  deriving (Eq, Show)
+
+
+instance FromJSON CKLookupResponse where
+  parseJSON = withObject "CKLookupResponse" $ \o ->
+    CKLookupResponse
+      <$> o .: "records"
+      <*> o .:? "syncToken"
+
+
+data CKZoneChangesZone = CKZoneChangesZone
+  { zczZoneId :: CKZoneId
+  , zczSyncToken :: Maybe Text
+  , zczMoreComing :: Maybe Bool
+  , zczRecords :: [CKRecord]
+  }
+  deriving (Eq, Show)
+
+
+instance FromJSON CKZoneChangesZone where
+  parseJSON = withObject "CKZoneChangesZone" $ \o ->
+    CKZoneChangesZone
+      <$> o .: "zoneID"
+      <*> o .:? "syncToken"
+      <*> o .:? "moreComing"
+      <*> o .:? "records" .!= []
+
+
+newtype CKZoneChangesResponse = CKZoneChangesResponse
+  { zcrZones :: [CKZoneChangesZone]
+  }
+  deriving (Eq, Show)
+
+
+instance FromJSON CKZoneChangesResponse where
+  parseJSON = withObject "CKZoneChangesResponse" $ \o ->
+    CKZoneChangesResponse <$> o .: "zones"
--- icloud-notes/test/ICloud/Notes/CloudKitSpec.hs
+++ icloud-notes/test/ICloud/Notes/CloudKitSpec.hs
@@ -1,7 +1,190 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module ICloud.Notes.CloudKitSpec (spec) where
 
+import Data.Aeson (eitherDecode)
+import qualified Data.ByteString.Lazy as LBS
+import qualified Data.Map.Strict as Map
+import Data.Text (Text)
+import Network.ICloud.Internal.Notes.CloudKit
+  ( CKField (..)
+  , CKLookupResponse (..)
+  , CKQueryResponse (..)
+  , CKRecord (..)
+  , CKRecordRef (..)
+  , CKZoneChangesResponse (..)
+  , CKZoneChangesZone (..)
+  )
 import Test.Hspec
 
 
 spec :: Spec
-spec = pure ()
+spec = describe "Network.ICloud.Internal.Notes.CloudKit" $ do
+  describe "CKQueryResponse" $ do
+    it "parses a folders query response" $ do
+      case eitherDecode queryFoldersJson :: Either String CKQueryResponse of
+        Left err -> expectationFailure err
+        Right r -> do
+          qrContinuationMarker r `shouldBe` Nothing
+          case qrRecords r of
+            [] -> expectationFailure "expected non-empty records"
+            rec : _ -> do
+              crName rec `shouldBe` "Folder/FOLDER-FIXTURE"
+              crType rec `shouldBe` Just "SearchIndexes"
+              Map.lookup "HasSubfolder" (crFields rec)
+                `shouldBe` Just (CKInt64Field 1)
+    it "parses an empty query response" $ do
+      case eitherDecode emptyQueryJson :: Either String CKQueryResponse of
+        Left err -> expectationFailure err
+        Right r -> qrRecords r `shouldBe` []
+
+  describe "CKLookupResponse" $ do
+    it "parses a note lookup response" $ do
+      case eitherDecode lookupNoteJson :: Either String CKLookupResponse of
+        Left err -> expectationFailure err
+        Right r -> do
+          lrSyncToken r `shouldBe` Just "notes-lookup-sync-token-fixture"
+          case lrRecords r of
+            [] -> expectationFailure "expected non-empty records"
+            rec : _ -> do
+              crName rec `shouldBe` "Note/NOTE-FIXTURE"
+              Map.lookup "TextDataEncrypted" (crFields rec)
+                `shouldBe` Just (CKEncryptedBytesField "c3ludGhldGljIG5vdGUgYm9keQ==")
+              Map.lookup "Folder" (crFields rec)
+                `shouldBe` Just (CKReferenceField (mkRef "Folder/FOLDER-FIXTURE"))
+              Map.lookup "Attachments" (crFields rec)
+                `shouldBe` Just (CKReferenceListField [mkRef "Attachment/ATTACHMENT-FIXTURE"])
+    it "parses an attachment lookup response" $ do
+      case eitherDecode lookupAttachmentJson :: Either String CKLookupResponse of
+        Left err -> expectationFailure err
+        Right r -> do
+          lrSyncToken r `shouldBe` Nothing
+          case lrRecords r of
+            [] -> expectationFailure "expected non-empty records"
+            rec : _ -> do
+              crName rec `shouldBe` "Attachment/ATTACHMENT-FIXTURE"
+              Map.lookup "AttachmentUTI" (crFields rec)
+                `shouldBe` Just (CKStringField "public.url")
+              Map.lookup "Size" (crFields rec)
+                `shouldBe` Just (CKInt64Field 128)
+
+  describe "CKZoneChangesResponse" $ do
+    it "parses zone changes with records" $ do
+      case eitherDecode zoneChangesJson :: Either String CKZoneChangesResponse of
+        Left err -> expectationFailure err
+        Right r ->
+          case zcrZones r of
+            [] -> expectationFailure "expected non-empty zones"
+            zone : _ -> do
+              zczSyncToken zone `shouldBe` Just "notes-zone-sync-token-fixture"
+              zczMoreComing zone `shouldBe` Just False
+              case zczRecords zone of
+                [_, _, deleted] -> do
+                  crName deleted `shouldBe` "Note/NOTE-DELETED-FIXTURE"
+                  crDeleted deleted `shouldBe` Just True
+                recs -> expectationFailure $ "expected 3 records, got " <> show (length recs)
+    it "parses zone changes with no records" $ do
+      case eitherDecode zoneChangesEmptyJson :: Either String CKZoneChangesResponse of
+        Left err -> expectationFailure err
+        Right r ->
+          case zcrZones r of
+            [] -> expectationFailure "expected non-empty zones"
+            zone : _ -> do
+              zczRecords zone `shouldBe` []
+              zczSyncToken zone `shouldBe` Just "notes-changes-sync-token-fixture"
+
+
+-- Helpers
+
+mkRef :: Text -> CKRecordRef
+mkRef name = CKRecordRef{rrRecordName = name, rrAction = "VALIDATE"}
+
+
+-- Fixtures
+
+queryFoldersJson :: LBS.ByteString
+queryFoldersJson =
+  "{\"records\":[{\"recordName\":\"Folder/FOLDER-FIXTURE\"\
+  \,\"recordType\":\"SearchIndexes\"\
+  \,\"recordChangeTag\":\"folder-change-tag-fixture\"\
+  \,\"zoneID\":{\"zoneName\":\"Notes\",\"zoneType\":\"REGULAR_CUSTOM_ZONE\"}\
+  \,\"fields\":{\"TitleEncrypted\":{\"type\":\"STRING\",\"value\":\"Synthetic Folder\",\"isEncrypted\":true}\
+  \,\"HasSubfolder\":{\"type\":\"INT64\",\"value\":1}}}]\
+  \,\"continuationMarker\":null}"
+
+
+emptyQueryJson :: LBS.ByteString
+emptyQueryJson = "{\"records\":[]}"
+
+
+lookupNoteJson :: LBS.ByteString
+lookupNoteJson =
+  "{\"records\":[{\"recordName\":\"Note/NOTE-FIXTURE\"\
+  \,\"recordType\":\"Note\"\
+  \,\"recordChangeTag\":\"note-change-tag-fixture\"\
+  \,\"created\":{\"timestamp\":1735689600000,\"userRecordName\":\"_synthetic_user\"}\
+  \,\"modified\":{\"timestamp\":1735776000000,\"userRecordName\":\"_synthetic_user\"}\
+  \,\"deleted\":false\
+  \,\"zoneID\":{\"zoneName\":\"Notes\",\"zoneType\":\"REGULAR_CUSTOM_ZONE\"}\
+  \,\"fields\":{\
+  \\"TitleEncrypted\":{\"type\":\"STRING\",\"value\":\"Synthetic note\",\"isEncrypted\":true}\
+  \,\"SnippetEncrypted\":{\"type\":\"STRING\",\"value\":\"Synthetic snippet\",\"isEncrypted\":true}\
+  \,\"TextDataEncrypted\":{\"type\":\"ENCRYPTED_BYTES\",\"value\":\"c3ludGhldGljIG5vdGUgYm9keQ==\"}\
+  \,\"ModificationDate\":{\"type\":\"TIMESTAMP\",\"value\":1735776000000}\
+  \,\"Deleted\":{\"type\":\"INT64\",\"value\":0}\
+  \,\"Folder\":{\"type\":\"REFERENCE\",\"value\":{\"recordName\":\"Folder/FOLDER-FIXTURE\",\"action\":\"VALIDATE\"}}\
+  \,\"Attachments\":{\"type\":\"REFERENCE_LIST\",\"value\":[{\"recordName\":\"Attachment/ATTACHMENT-FIXTURE\",\"action\":\"VALIDATE\"}]}\
+  \}}]\
+  \,\"syncToken\":\"notes-lookup-sync-token-fixture\"}"
+
+
+lookupAttachmentJson :: LBS.ByteString
+lookupAttachmentJson =
+  "{\"records\":[{\"recordName\":\"Attachment/ATTACHMENT-FIXTURE\"\
+  \,\"recordType\":\"Attachment\"\
+  \,\"recordChangeTag\":\"attachment-change-tag-fixture\"\
+  \,\"zoneID\":{\"zoneName\":\"Notes\",\"zoneType\":\"REGULAR_CUSTOM_ZONE\"}\
+  \,\"fields\":{\
+  \\"AttachmentIdentifier\":{\"type\":\"STRING\",\"value\":\"ATTACHMENT-ALIAS-FIXTURE\"}\
+  \,\"AttachmentUTI\":{\"type\":\"STRING\",\"value\":\"public.url\"}\
+  \,\"Filename\":{\"type\":\"STRING\",\"value\":\"synthetic-link.webloc\"}\
+  \,\"Size\":{\"type\":\"INT64\",\"value\":128}\
+  \,\"PrimaryAsset\":{\"type\":\"ASSETID\",\"value\":{\
+  \\"downloadURL\":\"https://example.test/notes/asset\"\
+  \,\"fileChecksum\":\"notes-asset-checksum-fixture\"\
+  \,\"referenceChecksum\":\"notes-asset-reference-fixture\"\
+  \,\"wrappingKey\":\"notes-asset-wrapping-key-fixture\"\
+  \,\"size\":128}}}}]}"
+
+
+zoneChangesJson :: LBS.ByteString
+zoneChangesJson =
+  "{\"zones\":[{\"zoneID\":{\"zoneName\":\"Notes\",\"zoneType\":\"REGULAR_CUSTOM_ZONE\"}\
+  \,\"syncToken\":\"notes-zone-sync-token-fixture\"\
+  \,\"moreComing\":false\
+  \,\"records\":[\
+  \{\"recordName\":\"Note/NOTE-FIXTURE\"\
+  \,\"recordType\":\"Note\"\
+  \,\"recordChangeTag\":\"note-change-tag-fixture\"\
+  \,\"fields\":{\
+  \\"TitleEncrypted\":{\"type\":\"STRING\",\"value\":\"Synthetic note\",\"isEncrypted\":true}\
+  \,\"SnippetEncrypted\":{\"type\":\"STRING\",\"value\":\"Synthetic snippet\",\"isEncrypted\":true}\
+  \,\"ModificationDate\":{\"type\":\"TIMESTAMP\",\"value\":1735776000000}\
+  \,\"Deleted\":{\"type\":\"INT64\",\"value\":0}\
+  \,\"Folder\":{\"type\":\"REFERENCE\",\"value\":{\"recordName\":\"Folder/FOLDER-FIXTURE\",\"action\":\"VALIDATE\"}}}}\
+  \,{\"recordName\":\"Folder/FOLDER-FIXTURE\"\
+  \,\"recordType\":\"SearchIndexes\"\
+  \,\"recordChangeTag\":\"folder-change-tag-fixture\"\
+  \,\"fields\":{\
+  \\"TitleEncrypted\":{\"type\":\"STRING\",\"value\":\"Synthetic Folder\",\"isEncrypted\":true}\
+  \,\"HasSubfolder\":{\"type\":\"INT64\",\"value\":1}}}\
+  \,{\"recordName\":\"Note/NOTE-DELETED-FIXTURE\",\"deleted\":true}\
+  \]}]}"
+
+
+zoneChangesEmptyJson :: LBS.ByteString
+zoneChangesEmptyJson =
+  "{\"zones\":[{\"zoneID\":{\"zoneName\":\"Notes\",\"zoneType\":\"REGULAR_CUSTOM_ZONE\"}\
+  \,\"records\":[]\
+  \,\"syncToken\":\"notes-changes-sync-token-fixture\"\
+  \,\"moreComing\":false}]}"
