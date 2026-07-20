4565cc51fedf8852bcbf6ed4bcffe98d1c877706
Tim Emiola <adetokunbo@users.noreply.github.com>
2026-07-20 12:22:52 +0900

    Extract module N.I.Internal.Http.Signin

--- icloud-auth/src/Network/ICloud/Http.hs
+++ icloud-auth/src/Network/ICloud/Http.hs
@@ -3,7 +3,6 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE MultiWayIf #-}
 {-# LANGUAGE NamedFieldPuns #-}
-{-# LANGUAGE OverloadedLists #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeFamilies #-}
@@ -77,92 +76,41 @@ module Network.ICloud.Http
 where
 
 import Control.Exception (IOException, catch, throwIO)
-import Control.Monad (unless, void)
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Trans.Reader (ReaderT, ask, asks, runReaderT)
-import Crypto.SRP
-  ( FromClient (..)
-  , FromServer (..)
-  , Results (..)
-  , calcResults
-  , mkFromClient
-  )
-import Data.Aeson
-  ( FromJSON (..)
-  , Object
-  , encode
-  , withObject
-  , (.:)
-  )
-import Data.Aeson.Types (Parser, Value (..), parseEither)
-import Data.Base64.Types (extractBase64)
-import Data.ByteString (ByteString)
-import Data.ByteString.Base64 (decodeBase64Untyped, encodeBase64)
-import Data.Maybe (fromMaybe)
-import Data.Text (Text)
+import Crypto.SRP (calcResults, mkFromClient)
+import Data.Aeson (FromJSON (..))
+import Data.Aeson.Types (Value (..), parseEither)
 import qualified Data.Text as Text
-import Data.Text.Encoding (encodeUtf8)
-import Data.Word (Word64, Word8)
-import Network.HTTP.Client
-  ( Request (..)
-  , Response (..)
-  )
-import Network.HTTP.Types
-  ( Status (..)
-  )
-import Network.ICloud.Http.Endpoints (Endpoints (..))
-import Network.ICloud.Internal.Endpoints
-  ( accountLoginBase
-  , extendPath
-  , homeHeaders
-  , listDevices
-  , sendVerification
-  , signinCompleteBase
-  , signinInitBase
-  , toPut
-  , twoFaOptionsBase
-  , twoSvTrust
-  , validateBase
-  , validateVerification
-  , verifySecurityCodeReq
-  , withAcceptJson
-  , withBody
-  , withHeaders
-  )
-import Network.ICloud.Internal.Http
-  ( KeyDeriver (..)
-  , PasswordProtocol (..)
-  , SrpContext (..)
-  , phoneCodeBody
-  , phoneTriggerBody
-  , validateSetupBody
-  )
+import Data.Word (Word8)
+import Network.ICloud.Internal.Http (SrpContext (..))
 import Network.ICloud.Internal.Http.Api
   ( Api (..)
   , ApiLogger
   , AuthCode
-  , asObject
-  , authHeaders
-  , callApi
-  , callHandlingResponse
-  , callRequiredHeaders
-  , extractOr'
   , fileLogger
-  , maybeValue
   , mkApi
   , mkApiWith
-  , mkJsonRequest
   , rawRequest
-  , requiredHeaders
-  , showStatusOf
   , verboseLogger
-  , withJsonRequestHeaders
   , withLogger
   )
-import Network.ICloud.Internal.HttpErrors
-  ( ApiResponse
-  , AuthError (..)
+import Network.ICloud.Internal.Http.Signin
+  ( accountLogin
+  , doTrustStep
+  , fetchTrustData
+  , listSetupDevices
+  , requestSmsCode
+  , runSigninComplete
+  , runSigninInit
+  , sendSetupVerification
+  , triggerTwoFaPush
+  , validate
+  , validateSetupVerification
+  , verifySmsCode
+  , verifyTwoFaCode
   )
+import Network.ICloud.Internal.HttpErrors (AuthError (..))
 import Network.ICloud.Internal.LoginFSM
   ( AfterAcctLogin (..)
   , AfterArtifactDir (..)
@@ -184,8 +132,7 @@ import Network.ICloud.Internal.LoginFSM
   , twoSaProcess
   )
 import Network.ICloud.Internal.Session
-  ( SavedHeaders (..)
-  , accountDataRequires2FA
+  ( accountDataRequires2FA
   , accountDataRequires2SA
   , loadAccountData
   , loadSavedHeaders
@@ -396,47 +343,6 @@ parseAccountData v =
     parseEither parseJSON v
 
 
--- | Fetch the 2FA options immediately after the 409 from signin/complete
-fetchTrustData :: Api -> IO TrustData
-fetchTrustData api = do
-  savedHdrs <- loadSavedHeaders (apiSession api)
-  let req = withHeaders (withAcceptJson $ authHeaders api savedHdrs) (twoFaOptionsBase (apiEndpoints api))
-  callApi api req >>= extractOr'
-
-
--- | POST to phone/securitycode to request an SMS code to the given phone
-requestSmsCode :: Api -> TrustedPhone -> IO ()
-requestSmsCode api@Api{apiEndpoints = ep} tp = do
-  savedHdrs <- loadSavedHeaders (apiSession api)
-  let req =
-        withHeaders (authHeaders api savedHdrs) $
-          withJsonRequestHeaders $
-            withBody (encode $ phoneTriggerBody tp) $
-              toPut (extendPath (epAuth ep) "/verify/phone")
-  resp <- rawRequest api req
-  unless (statusCode (responseStatus resp) < 400) $
-    throwIO $
-      UnexpectedResponse $
-        showStatusOf resp
-
-
--- | POST to phone/securitycode to verify an SMS code; returns True when accepted
-verifySmsCode :: Api -> TrustedPhone -> AuthCode -> IO Bool
-verifySmsCode api@Api{apiEndpoints = ep} tp code = do
-  savedHdrs <- loadSavedHeaders (apiSession api)
-  let req =
-        withHeaders (authHeaders api savedHdrs) $
-          withJsonRequestHeaders $
-            withBody (encode $ phoneCodeBody tp code) $
-              verifySecurityCodeReq "phone" ep
-  resp <- rawRequest api req
-  let c = statusCode (responseStatus resp)
-  if
-    | c < 400 -> pure True
-    | c == 400 -> pure False
-    | otherwise -> throwIO $ UnexpectedResponse $ showStatusOf resp
-
-
 -- | Complete a pending 2FA (auth-endpoint) challenge
 completeTwoFactor :: Api -> IO AuthState
 completeTwoFactor api = completeTwoFactorWith pleaseReadCode (\_ -> pure Nothing) api
@@ -475,248 +381,3 @@ complete2SAWith pickDevice readCode api devices = do
     CompletionNeedsTwoFa _ -> throwIO TwoFactorStillRequired
     CompletionNeedsTwoSa (TwoSaReady _ ds) -> pure $ Requires2SA (apiSession api) ds
     CompletionTwoFaLocked _ -> throwIO TwoFactorLocked
-
-
-data SigninInitReply = SigninInitReply
-  { sirTag :: !Text
-  , sirProtocol :: !PasswordProtocol
-  , sirPublicBytes :: !ByteString
-  , sirIterations :: !Word64
-  , sirSalt :: !ByteString
-  }
-  deriving (Eq, Show)
-
-
-instance FromJSON SigninInitReply where
-  parseJSON = withObject "SigninInitReply" parseSigninInitReply
-
-
-parseSigninInitReply :: Object -> Parser SigninInitReply
-parseSigninInitReply o =
-  let tag = o .: "c"
-      iterations = o .: "iteration"
-      protocol = o .: "protocol"
-      publicBytes = o .: "b" >>= parseBase64Bytes
-      salt = o .: "salt" >>= parseBase64Bytes
-      parseBase64Bytes s = case decodeBase64Untyped (encodeUtf8 s) of
-        Left err -> fail $ Text.unpack err
-        Right b -> pure b
-   in SigninInitReply
-        <$> tag
-        <*> protocol
-        <*> publicBytes
-        <*> iterations
-        <*> salt
-
-
-signinInit :: Api -> FromClient -> IO SigninInitReply
-signinInit api other = do
-  savedHdrs <- loadSavedHeaders (apiSession api)
-  callHandlingResponse signinInitReq (withHeaders (authHeaders api savedHdrs)) api other
-
-
-runSigninInit :: Api -> FromClient -> IO (FromServer, KeyDeriver)
-runSigninInit api fc = do
-  r <- signinInit api fc
-  let fromServer =
-        FromServer
-          { fsPublicBytes = sirPublicBytes r
-          , fsSalt = sirSalt r
-          , fsPrimeGroup = apiGroup api
-          , fsKnownAlgorithm = apiHashAlgorithm api
-          }
-      keyDeriver =
-        KeyDeriver
-          { kdTag = sirTag r
-          , kdIterations = sirIterations r
-          , kdWrappedF = apiWrappedPseudoRF api
-          , kdProtocol = sirProtocol r
-          }
-  pure (fromServer, keyDeriver)
-
-
-signinInitReq :: Endpoints -> FromClient -> Request
-signinInitReq = mkJsonRequest signinInitBase signinInitValue
-
-
-signinInitValue :: FromClient -> Value
-signinInitValue fc =
-  let a = extractBase64 $ encodeBase64 $ fcPublicBytes fc
-   in asObject
-        [ ("a", String a)
-        , ("accountName", String (fcUser fc))
-        , ("protocols", Array ["s2k", "s2k_fo"])
-        ]
-
-
-data SigninCompletion = SigninCompletion
-  { siTag :: !Text
-  , siAccountName :: !Text
-  , siSavedHeaders :: !SavedHeaders
-  , siResults :: !Results
-  }
-
-
-runSigninComplete :: Api -> KeyDeriver -> Results -> IO ()
-runSigninComplete api@Api{apiSession = session} kd results = do
-  siSavedHeaders <- loadSavedHeaders session
-  let siAccountName = credAccountName $ sessionCreds session
-      completion =
-        SigninCompletion
-          { siTag = kdTag kd
-          , siAccountName
-          , siResults = results
-          , siSavedHeaders
-          }
-  signinComplete api completion
-
-
-signinComplete :: Api -> SigninCompletion -> IO ()
-signinComplete api sc = do
-  savedHdrs <- loadSavedHeaders (apiSession api)
-  let req = withHeaders (authHeaders api savedHdrs) $ signinCompleteReq (apiEndpoints api) sc
-  resp <- callApi api req :: IO (Response (ApiResponse ()))
-  handleSigninComplete resp
-
-
-signinCompleteReq :: Endpoints -> SigninCompletion -> Request
-signinCompleteReq = mkJsonRequest signinCompleteBase signinCompleteValue
-
-
-signinCompleteValue :: SigninCompletion -> Value
-signinCompleteValue sc =
-  let Results{rClientProof, rServerProof} = siResults sc
-      toBase64 = extractBase64 . encodeBase64
-      singleElem x = Array [String x]
-      maybeArray = maybe (Array []) singleElem
-   in asObject
-        [ ("m1", String (toBase64 rClientProof))
-        , ("m2", String (toBase64 rServerProof))
-        , ("trustTokens", maybeArray (shTrustToken (siSavedHeaders sc)))
-        , ("rememberMe", Bool True)
-        , ("accountName", String (siAccountName sc))
-        , ("c", String (siTag sc))
-        ]
-
-
-handleSigninComplete :: Response (ApiResponse ()) -> IO ()
-handleSigninComplete resp = do
-  let code = statusCode $ responseStatus resp
-  if
-    | code == 401 -> throwIO InvalidCredentials
-    | code == 403 -> throwIO AccountLocked
-    | code == 412 -> throwIO PrivacyAgreementRequired
-    | code == 409 -> pure () -- 2FA required; accountLogin will detect it
-    | code >= 400 -> throwIO $ UnexpectedResponse $ showStatusOf resp
-    | otherwise -> pure ()
-
-
-validate :: Api -> IO Bool
-validate api@Api{apiEndpoints} = do
-  resp <- rawRequest api (validateReq apiEndpoints)
-  let code = statusCode (responseStatus resp)
-  if
-    | code == 401 -> pure False
-    | code >= 400 -> throwIO $ UnexpectedResponse $ showStatusOf resp
-    | otherwise -> pure True
-
-
-validateReq :: Endpoints -> Request
-validateReq = withJsonRequestHeaders . withBody (encode Null) . validateBase
-
-
-accountLogin :: Api -> IO Value
-accountLogin api@Api{apiEndpoints = ep} = do
-  savedHdrs <- loadSavedHeaders $ apiSession api
-  let hdrs = homeHeaders ep
-  callHandlingResponse accountLoginReq (withHeaders hdrs) api savedHdrs
-
-
-accountLoginReq :: Endpoints -> SavedHeaders -> Request
-accountLoginReq = mkJsonRequest accountLoginBase accountLoginValue
-
-
-triggerTwoFaPush :: Api -> IO ()
-triggerTwoFaPush api@Api{apiEndpoints = ep} = do
-  savedHdrs <- loadSavedHeaders (apiSession api)
-  let req =
-        withHeaders
-          (withAcceptJson $ authHeaders api savedHdrs)
-          (toPut (extendPath (epAuth ep) "/verify/trusteddevice/securitycode"))
-  -- The server sometimes returns a non-2xx here when a push is already in flight;
-  -- the device still shows the notification, so errors are safe to ignore.
-  void (rawRequest api req) `catch` \(_ :: IOException) -> pure ()
-
-
-doTrustStep :: Api -> IO ()
-doTrustStep api@Api{apiEndpoints = ep} = do
-  savedHdrs <- loadSavedHeaders (apiSession api)
-  let req = withHeaders (withAcceptJson $ authHeaders api savedHdrs) (twoSvTrust ep)
-  resp <- rawRequest api req
-  unless (statusCode (responseStatus resp) < 400) $
-    throwIO $
-      UnexpectedResponse $
-        showStatusOf resp
-
-
-verifyTwoFaCode :: Api -> AuthCode -> IO Bool
-verifyTwoFaCode api@Api{apiEndpoints = ep} code = do
-  savedHdrs <- loadSavedHeaders (apiSession api)
-  let body = encode $ Object [("securityCode", Object [("code", String code)])]
-      req =
-        withHeaders (authHeaders api savedHdrs) $
-          withJsonRequestHeaders $
-            withBody body $
-              verifySecurityCodeReq "trusteddevice" ep
-  resp <- rawRequest api req
-  let c = statusCode (responseStatus resp)
-  if
-    | c < 400 -> pure True
-    | c == 400 -> pure False
-    | otherwise -> throwIO $ UnexpectedResponse $ showStatusOf resp
-
-
-accountLoginValue :: SavedHeaders -> Value
-accountLoginValue hs =
-  asObject
-    [ ("accountCountryCode", maybeValue String (shCountry hs))
-    , ("dsWebAuthToken", maybeValue String (shSessionToken hs))
-    , ("trustToken", String $ fromMaybe "" $ shTrustToken hs)
-    , ("extended_login", Bool True)
-    ]
-
-
-newtype ListDevicesReply = ListDevicesReply {ldrDevices :: [Setup2SADevice]}
-
-
-instance FromJSON ListDevicesReply where
-  parseJSON = withObject "ListDevicesReply" $ \o -> ListDevicesReply <$> o .: "devices"
-
-
-listSetupDevices :: Api -> IO [Setup2SADevice]
-listSetupDevices api@Api{apiEndpoints = ep} =
-  ldrDevices <$> callRequiredHeaders api (listDevices ep)
-
-
-sendSetupVerification :: Api -> Setup2SADevice -> IO ()
-sendSetupVerification api@Api{apiSession = s, apiEndpoints = ep} device = do
-  savedHdrs <- loadSavedHeaders s
-  let req =
-        withHeaders (requiredHeaders savedHdrs) $
-          withJsonRequestHeaders $
-            withBody (encode device) $
-              sendVerification ep
-  resp <- rawRequest api req
-  unless (statusCode (responseStatus resp) < 400) $ throwIO $ UnexpectedResponse $ showStatusOf resp
-
-
-validateSetupVerification :: Api -> Setup2SADevice -> AuthCode -> IO Bool
-validateSetupVerification api@Api{apiSession = s, apiEndpoints = ep} device code = do
-  savedHdrs <- loadSavedHeaders s
-  let req =
-        withHeaders (requiredHeaders savedHdrs) $
-          withJsonRequestHeaders $
-            withBody (encode $ validateSetupBody device code) $
-              validateVerification ep
-  resp <- rawRequest api req
-  pure $ statusCode (responseStatus resp) < 400
