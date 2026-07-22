1988c4af31793e4302b1e5c88ea9276d89892842
Tim Emiola <adetokunbo@users.noreply.github.com>
2026-07-23 07:22:16 +0900

    Rename package directories, update cabal.project, CI, and READMEs to hstratus-*

---
+++ hstratus-auth/src/Network/HStratus/Http/Common.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+{- |
+Module      : Network.HStratus.Http.Common
+Copyright   : (c) 2025 Tim Emiola
+Maintainer  : Tim Emiola <adetokunbo@emio.la>
+SPDX-License-Identifier: BSD3
+
+Shared HTTP utilities for iCloud service clients.
+
+Re-exported from "Network.HStratus.Internal.Endpoints" for use by downstream
+libraries (@hstratus-drive@, @hstratus-notes@, etc.) that need to build
+authenticated service requests.
+-}
+module Network.HStratus.Http.Common
+  ( -- * Headers
+    icloudHome
+  , icloudBrowserHeaders
+  , withHeaders
+
+    -- * Request helpers
+  , stripTrailingSlash
+  , lookupWebservice
+  )
+where
+
+import Network.HStratus.Internal.Endpoints
+  ( icloudBrowserHeaders
+  , icloudHome
+  , lookupWebservice
+  , stripTrailingSlash
+  , withHeaders
+  )
+
---
+++ hstratus-notes/app/CmdLine.hs
@@ -0,0 +1,121 @@
+module Main where
+
+import Control.Exception (Exception, catch, displayException)
+import Data.List (find)
+import qualified Data.Text as Text
+import Network.HStratus.Http (Api, AuthError)
+import Network.HStratus.Http.Cli (CommonOpts (..), commonOptsParser, runWithApi)
+import Network.HStratus.Notes
+import Options.Applicative
+import System.Exit (exitFailure)
+
+
+data Command
+  = ListFolders CommonOpts
+  | ListNotes ListNotesOpts
+
+
+data ListNotesOpts = ListNotesOpts
+  { lnFolder :: Maybe Text.Text
+  , lnCommon :: CommonOpts
+  }
+
+
+commandParser :: Parser Command
+commandParser =
+  subparser
+    ( command
+        "list-note-folders"
+        ( info
+            (ListFolders <$> commonOptsParser <**> helper)
+            (progDesc "List all iCloud Notes folders")
+        )
+        <> command
+          "list-notes"
+          ( info
+              (ListNotes <$> listNotesOptsParser <**> helper)
+              (progDesc "List notes, optionally filtered by folder ID")
+          )
+    )
+
+
+listNotesOptsParser :: Parser ListNotesOpts
+listNotesOptsParser =
+  ListNotesOpts
+    <$> optional
+      ( Text.pack
+          <$> strOption
+            ( long "folder"
+                <> metavar "NAME"
+                <> help "Folder name (e.g. TukTuk)"
+            )
+      )
+    <*> commonOptsParser
+
+
+cliParser :: ParserInfo Command
+cliParser =
+  info
+    (commandParser <**> helper)
+    (fullDesc <> progDesc "hstratus-notes: iCloud Notes access tool")
+
+
+main :: IO ()
+main = do
+  cmd <- execParser cliParser
+  case cmd of
+    ListFolders opts -> runListFolders opts
+    ListNotes opts -> runListNotes opts
+
+
+runListFolders :: CommonOpts -> IO ()
+runListFolders opts =
+  withNotesApi opts $ \api ep ->
+    noteFolders api ep >>= mapM_ printFolder
+
+
+runListNotes :: ListNotesOpts -> IO ()
+runListNotes opts =
+  withNotesApi (lnCommon opts) $ \api ep -> do
+    notes <- case lnFolder opts of
+      Nothing -> recentNotes api ep
+      Just name -> do
+        fid <- resolveFolderName api ep name
+        notesInFolder api ep fid
+    mapM_ printNote notes
+
+
+resolveFolderName :: Api -> NotesEndpoints -> Text.Text -> IO FolderId
+resolveFolderName api ep name = do
+  folders <- noteFolders api ep
+  case find (matchesName name) folders of
+    Just nf -> pure (nfId nf)
+    Nothing -> do
+      putStrLn $ "No folder named '" <> Text.unpack name <> "'"
+      exitFailure
+ where
+  matchesName n nf = maybe False (\fn -> Text.toCaseFold fn == Text.toCaseFold n) (nfName nf)
+
+
+printFolder :: NoteFolder -> IO ()
+printFolder nf =
+  putStrLn $ Text.unpack (unFolderId (nfId nf)) <> nameStr
+ where
+  nameStr = maybe "" (("  " <>) . Text.unpack) (nfName nf)
+
+
+printNote :: NoteSummary -> IO ()
+printNote ns =
+  putStrLn $ Text.unpack (unNoteId (nsId ns)) <> titleStr
+ where
+  titleStr = maybe "" (("  " <>) . Text.unpack) (nsTitle ns)
+
+
+withNotesApi :: CommonOpts -> (Api -> NotesEndpoints -> IO ()) -> IO ()
+withNotesApi opts runAction =
+  runWithApi opts (\ad sess api -> mkNotesEndpoints ad sess >>= runAction api)
+    `catch` (\e -> onError (e :: AuthError))
+    `catch` (\e -> onError (e :: NotesError))
+ where
+  onError :: (Exception a) => a -> IO ()
+  onError e = putStrLn ("Error: " <> displayException e) >> exitFailure
